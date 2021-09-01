5e823b2d0a8348d13a9576f1acb0160ad781b6b3
Raghavendra Joshi <rjoshi@aasalabs.com>
2021-09-01 19:50:11 +0530

    Rails low level cache used to store the game status

--- app/controllers/rps_games_controller.rb
+++ app/controllers/rps_games_controller.rb
@@ -1,12 +1,35 @@
 class RpsGamesController < ApplicationController
-
+  before_action :clear_cache, :only => [:new]
+  
   def create
     response = RockPaperScissors.new(params[:player_choice]).play
     if response[:success].present?
-      redirect_to(display_rps_games_url + "?success=#{response[:success]}&message=#{response[:message]}&result=#{response[:data][:result]}&player-move=#{response[:data][:player_move]}&server-move=#{response[:data][:server_move]}")
+      set_success_cache(response)
     else
-      redirect_to(display_rps_games_url + "?success=#{response[:success]}&message=#{response[:message]}")
+      set_failure_cache(response)
     end
+    redirect_to display_rps_games_url
+  end
+
+  private
+
+  def clear_cache
+    ['success', 'message', 'result', 'player-move', 'server-move'].each do |key|
+      Rails.cache.delete(key)
+    end
+  end
+
+  def set_success_cache(response)
+    Rails.cache.write('success', response[:success])
+    Rails.cache.write('message', response[:message])
+    Rails.cache.write('result', response[:data][:result])
+    Rails.cache.write('player-move', response[:data][:player_move])
+    Rails.cache.write('server-move', response[:data][:server_move])
+  end
+
+  def set_failure_cache(response)
+    Rails.cache.write('success', response[:success])
+    Rails.cache.write('message', response[:message])
   end
 
 end
