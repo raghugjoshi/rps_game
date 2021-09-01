b7652619fea15e9f67ab99156e85db4c240e6b6f
Raghavendra Joshi <rjoshi@aasalabs.com>
2021-09-01 18:10:05 +0530

    jQuery is added to webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+const webpack = require('webpack');
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+ $: 'jquery',
+ jQuery: 'jquery'
+}));
+
+const config = environment.toWebpackConfig();
+config.resolve.alias = {
+ jquery: 'jquery/src/jquery'
+};
+
+module.exports = environment;
